01221244731c9ec453416041e79703846973491d
Nikita <niknakels@gmail.com>
2024-07-11 23:31:10 +1100

    Add files via upload

---
+++ namespace_lesson.py
@@ -0,0 +1,12 @@
+# Домашнее задание по уроку "Пространство имен"
+
+def test_function():
+    inner_function()
+
+
+def inner_function():
+    print('Я в области видимости функции test_function')
+
+
+test_function()
+inner_function()
